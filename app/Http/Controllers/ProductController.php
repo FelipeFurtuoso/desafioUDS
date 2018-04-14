485b5e4cf60f13fc0e83ff3711999f3f4bf5005a
Felipe Furtuoso <felipe.furtuoso538@gmail.com>
2018-04-14 15:01:09 -0300

    Controller Produto , Model Product e endpoint Produto

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    
+    protected $product = null;
     public function __construct()
     {
         
@@ -20,7 +20,20 @@ class ProductController extends Controller
      */
     public function index(Request $request)
     {
-
+        $product = new Product;
+        if (!empty($request['code'])) {
+            $getCode = $product->findProductByCode($request['code']);
+            foreach ($getCode as $key => $getCodeOk) {}
+                
+            return $getCodeOk;
+        }
+        if (!empty($request['name'])) {
+            $getName = $product->findProductByName($request['name']);
+            foreach ($getName as $key => $getNameOk) {}
+            return $getNameOk;
+        }
+        
+       return $product->allproducts();
     }
 
     /**
@@ -31,7 +44,17 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-    
+        
+        $product = new Product;
+
+        $existCode = $product->findProductByCode($request['code']);
+        $existName = $product->findProductByName($request['name']);
+        if (count($existCode) >= 1)return 'Codigo ja exite';
+        if (count($existName) >= 1)return 'Nome ja exite';
+        
+        $product->createProduct($request->all());
+        
+        return 'Cadastrado com sucesso';
     }
 
     /**
@@ -40,9 +63,13 @@ class ProductController extends Controller
      * @param  \App\Product  $product
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($code)
     {
-
+        $products = new Product();
+        $show = $products->findProductByCode($code);
+        if (count($show) == 0) return 'Produto nao existe';
+        
+        return $products->findProductByCode($code);
     }
 
     /**
@@ -55,6 +82,14 @@ class ProductController extends Controller
     public function update(Request $request,$id)
     {
 
+
+         $product = new Product;
+         $find = $product->findProductById($id);
+         if (is_null($find)) return 'Produto nao existe';
+         
+         $product->updateProduct($request->all(), $find->id);
+
+        return 'Atualizado com sucesso'; 
     }
 
     /**
@@ -63,8 +98,12 @@ class ProductController extends Controller
      * @param  \App\Product  $product
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy($code)
     {
-
+        $product = new Product;
+        $find = $product->findProductByCode($code);
+        if (is_null($find)) return 'Produto nao existe';
+        $product->deleteProduct($code);
+        return 'Deletado com sucesso'; 
     }
 }
--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,64 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-	
+	protected $fillable = [
+        
+        'code',
+        'name',
+        'price'
+    ];
+
+    public function allproducts ()
+    {
+    	return $this->all();	
+    }
+
+    public function createProduct(array $params) 
+    {
+      
+    	$product = new Product($params);
+    	$product->save();
+      return $product;
+    }
+
+    public function findProductById($id)
+    {
+       $product = self::find($id);
+
+       return $product;
+    }
+
+    public function findProductByCode($code)
+    {
+       $product = self::all()->where('code','=',$code);
+
+       return $product;
+    }
+
+    public function findProductByName($name)
+    {
+       
+       $product = self::all()->where('name','=',$name);
+
+       return self::all()->where('name','=',$name);
+    }
+
+    public function updateProduct($params , $id)
+    {
+        
+        $product = self::findProductById($id);
+        $product->fill($params);
+        $product->save();
+        return $product;
+
+    }
+
+    public function deleteProduct($code)
+    {
+       $product = self::findProductByCode($code);
+       $product->delete();
+       return $product;
+    }
+
+    
 }
